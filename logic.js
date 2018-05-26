f47ac94dbb6795ba40e3117054a9916eba4d0fc2
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-05-25 23:26:20 -0700

    Updating logic

--- logic.js
+++ logic.js
@@ -1,26 +1,42 @@
+function searchArticles() {
 
-var userinput=""
+  event.preventDefault();
+  
+  var userinput = $("#searchterm").val();
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "2d3d38f14f5248d787f0beae9073ce46",
-  'q': userinput
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-    //$('#search').text(JSON.stringify(response))
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "2d3d38f14f5248d787f0beae9073ce46",
+    'q': userinput
+  });
 
-$("#search").on("click", function(event) {
-    event.preventDefault();
+  console.log(url);
 
-    var userinput = $("#searchterm").val();
-    console.log(userinput);
-})
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    var response = result.response.docs;
+    var articleSpace = $("#articleSpace");
 
+    for ( var i = 0; i < response.length; i++) {
 
+      var articleDiv = $("<div class='article'>");
+
+      var headline = $("<h2>").text((i + 1) + " " + response[i].headline.main);
+      var byLine = $("<p>").text(response[i].byline.original);
+      var pubDate =  $("<p>").text(response[i].pub_date);
+      var webURL = $("<a>").text(response[i].web_url).attr("href", response[i].web_url).attr("target", "_blank");
+
+      articleDiv.append(headline, byLine, pubDate, webURL);
+
+      articleSpace.append(articleDiv);
+
+    }
+
+  }).fail(function(err) {
+    throw err;
+  });
+}
+
+$("#search").on("click", searchArticles);
